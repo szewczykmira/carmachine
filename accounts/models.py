9e49ca835613e4171fe41affdaf91aad36d302a4
Mira <szewczyk.mira@gmail.com>
2016-05-12 19:56:26 +0200

    First two models added

---
+++ accounts/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+
+class Account(User):
+    class Meta:
+        proxy = True
+
+class Employee(Account):
+   contract_begin = models.DateField(
+           verbose_name=_("Begin of contract"))
+   telephone = models.CharField(
+           verbose_name=_("Telephone"), max_length=9)
+   address = models.TextField(
+           verbose_name=_("Address"))
+   salary = models.FloatField(
+           verbose_name=_("Salary"))
+
+class Client(Account):
+    telephone = models.CharField(
+            verbose_name=_("Telephone"), max_length=9)
+    address = models.TextField(
+            verbose_name=_("Address"))
