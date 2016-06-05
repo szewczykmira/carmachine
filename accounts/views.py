488989dede00055550be177f2e9641c65e61ac53
Mira <szewczyk.mira@gmail.com>
2016-06-05 14:42:59 +0200

    Error handling added

--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,12 @@ from . import forms
 def add_user(request, is_client=False):
     context = {'add_user': True}
     if is_client:
-        context['form'] = forms.ClientForm()
+        context['form'] = forms.ClientForm(request.POST or None)
     else:
-        context['form'] = forms.EmployeeForm()
+        context['form'] = forms.EmployeeForm(request.POST or None)
+    if request.POST:
+        if context['form'].is_valid():
+            #user = context['form'].save()
+            print "Udalo sie"
     return render(request, "accounts/add_user.html", context=context)
 
