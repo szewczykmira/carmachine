bbe1457d9efe049afbd79f312a927dce4fa19535
Mira <szewczyk.mira@gmail.com>
2016-06-10 20:03:28 +0200

    Delete object function moved to other file

---
+++ static/js/delete_object.js
@@ -0,0 +1,25 @@
+/**
+ * Created by mira on 6/10/16.
+ */
+
+var delete_employee_id = null;
+$('.removeObject').on('click', function(){
+    delete_employee_id = $(this).data('id');
+});
+
+$('#deleteModal').on('click', function(){
+    $.ajax({
+        type: "POST",
+        url: delete_url,
+        data: {
+            'csrfmiddlewaretoken': csrf,
+            'object_id': delete_employee_id
+        },
+        success: function(data){
+            if(data.success){
+                $('#object-'+delete_employee_id).hide();
+            }
+        }
+    });
+    $('#deleteModal').modal('hide');
+});
--- static/js/employee_table.js
+++ static/js/employee_table.js
@@ -47,25 +47,3 @@ $('#modalSend').on('click', function(){
     });
     $('#salaryModal').modal('hide');
 });
-
-var delete_employee_id = null;
-$('.removeObject').on('click', function(){
-    delete_employee_id = $(this).data('id');
-});
-
-$('#deleteModal').on('click', function(){
-    $.ajax({
-        type: "POST",
-        url: delete_url,
-        data: {
-            'csrfmiddlewaretoken': csrf,
-            'object_id': delete_employee_id
-        },
-        success: function(data){
-            if(data.success){
-                $('#object-'+delete_employee_id).hide();
-            }
-        }
-    });
-    $('#deleteModal').modal('hide');
-});
