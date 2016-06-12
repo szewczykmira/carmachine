816359f263df93fc2ba4b782bdfbc83054e602a9
Mira <szewczyk.mira@gmail.com>
2016-06-12 16:25:10 +0200

    Display order

--- order/urls.py
+++ order/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.home, name="order_home"),
     url(r'add/$', views.add_order, name="order_add"),
     url(r'edit/(?P<order_id>\d+)/$', views.add_order, name="order_edit"),
+    url(r'(?P<order_id>\d+)/$', views.display_order, name="order_display"),
     url(r'delete/$', views.delete_order, name="order_delete"),
 ]
--- order/views.py
+++ order/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.http import Http404, HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -33,7 +34,8 @@ def add_order(request, order_id=None):
         if context['form'].is_valid():
             order = context['form'].save()
             messages.success(request, _('Order has been added'))
-            return redirect('order_home')
+            return redirect(reverse('order_display',
+                                    kwargs={'order_id': order.id}))
         messages.error(request, _("Please review information!"))
 
     return render(request, 'order/add_order.html', context)
@@ -42,3 +44,13 @@ def add_order(request, order_id=None):
 @login_required(login_url='/accounts/login')
 def delete_order(request):
     return delete_view(request, models.Order)
+
+
+@login_required(login_url='/accounts/login')
+def display_order(request, order_id):
+    if Account.objects.get_from_user(request.user).is_client() or \
+            not request.user.is_active:
+        return Http404(_("You are not allowed to be here!"))
+
+    context = {'order': get_object_or_404(models.Order, id=order_id)}
+    return render(request, 'order/display_order.html', context)
