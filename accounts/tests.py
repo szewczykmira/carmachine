137db4a97339586bb04646658dcdd0a0599e1762
Mira <szewczyk.mira@gmail.com>
2016-07-25 23:07:28 +0200

    New test added

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,7 +4,10 @@ from django.test import TestCase
 from accounts.models import Client, Employee
 from django.contrib.auth.models import User
 
+TELEPHONE = "123456789"
+
 class AccountsTestCase(TestCase):
+
     def create_user(self, model, login, mail, password, l_name, 
             f_name, active):
         user = User.objects.create_user(login, mail, password)
@@ -13,10 +16,10 @@ class AccountsTestCase(TestCase):
         user.is_active = active
         user.save()
         if model == "employee":
-            Employee.objects.create(account=user, telephone="123456789",
+            Employee.objects.create(account=user, telephone=TELEPHONE,
                     address="Null", contract_begin="3020-12-12")
         else:
-            Client.objects.create(account=user, telephone="123456789",
+            Client.objects.create(account=user, telephone=TELEPHONE,
                     address="Null")
     
     def setUp(self):
@@ -42,3 +45,14 @@ class AccountsTestCase(TestCase):
         employee = Employee.objects.get(address="Null")
         self.assertEqual(user.employee, employee)
         self.assertEqual(user, employee.account)
+
+    def test_create_employee(self):
+        user = User.objects.create_user("charlie", "charlie@nassau.com", "Eleonora")
+        employee_count = Employee.objects.count()
+        employee = Employee.objects.create(account=user, telephone=TELEPHONE,
+                address="Null", contract_begin="2020-12-12")
+        self.assertEqual("charlie", employee.account.username)
+        self.assertEqual(employee_count + 1, Employee.objects.count())
+
+    def test_create_client(self):
+        pass
